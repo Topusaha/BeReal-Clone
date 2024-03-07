e707527de40474dbedac5ee3e8c669cdac138ee0
Topu Saha <topusaha@Topus-Air.lan>
2024-03-07 08:54:51 -0500

    Login Screen Laid out

---
+++ BeRealClone/BeRealClone/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  BeRealClone
+//
+//  Created by Topu Saha on 3/7/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+
+}
